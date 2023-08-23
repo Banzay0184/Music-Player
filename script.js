30820a4565cd74f5681723df2449085538165c9e
Banzay0184 <shakhzodabidov@gmail.com>
2023-08-23 17:52:28 +0500

    0.2

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let prev_btn = document.querySelector('.prev-track');
 
 let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
+let btnVol = document.querySelectorAll('.btn-vol')
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
 let wave = document.getElementById('wave');
@@ -22,34 +23,82 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img: 'images/stay.png',
+        name: 'Stay',
+        artist: 'The Kid LAROI, Justin Bieber',
+        music: 'music/stay.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        img: 'images/fallingdown.jpg',
+        name: 'Falling Down',
+        artist: 'Wid Cards',
+        music: 'music/fallingdown.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        img: 'images/faded.png',
+        name: 'Faded',
+        artist: 'Alan Walker',
+        music: 'music/Faded.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        img: 'images/ratherbe.jpg',
+        name: 'Rather Be',
+        artist: 'Clean Bandit',
+        music: 'music/Rather Be.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Hani Benim Gen',
+        artist: 'Ahmet Kaya',
+        music: 'music/Hani Benim Gen.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Mos HayLife Hayastans',
+        artist: 'Edo',
+        music: 'music/Mos HayLife Hayastans.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Makeba Jain',
+        artist: '',
+        music: 'music/Makeba Jain.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Hyat Mni Aladr',
+        artist: 'Murad Adaml',
+        music: 'music/Hyat Mni Aladr Gldrmr Niy.m4a'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Reza Malekzadeh Aram Aram',
+        artist: '',
+        music: 'music/Reza Malekzadeh Aram Aram.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Где ты теперь',
+        artist: 'Баста,HammAli,Navai',
+        music: 'music/Где ты теперь.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Где нас нет',
+        artist: 'Баста,Градусы',
+        music: 'music/Где нас нет.mp3'
+    },
+    {
+        img: 'https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=',
+        name: 'Я по кличке Принц',
+        artist: 'Киракосян Бродяга',
+        music: 'music/Я по кличке Принц не хулиган.mp3'
     }
 ];
 
 loadTrack(track_index);
 
-function loadTrack(track_index){
+function loadTrack(track_index) {
     clearInterval(updateTimer);
     reset();
 
@@ -67,92 +116,120 @@ function loadTrack(track_index){
     random_bg_color();
 }
 
-function random_bg_color(){
+function random_bg_color() {
     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
     let a;
 
-    function populate(a){
-        for(let i=0; i<6; i++){
+    function populate(a) {
+        for (let i = 0; i < 6; i++) {
             let x = Math.round(Math.random() * 14);
             let y = hex[x];
             a += y;
         }
         return a;
     }
+
     let Color1 = populate('#');
     let Color2 = populate('#');
     var angle = 'to right';
 
 }
-function reset(){
+
+function reset() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
-function randomTrack(){
+
+function randomTrack() {
     isRandom ? pauseRandom() : playRandom();
 }
-function playRandom(){
+
+function playRandom() {
     isRandom = true;
     randomIcon.classList.add('randomActive');
 }
-function pauseRandom(){
+
+function pauseRandom() {
     isRandom = false;
     randomIcon.classList.remove('randomActive');
 }
-function repeatTrack(){
+
+function repeatTrack() {
     let current_index = track_index;
     loadTrack(current_index);
     playTrack();
 }
-function playpauseTrack(){
+
+function playpauseTrack() {
     isPlaying ? pauseTrack() : playTrack();
 }
-function playTrack(){
+
+function playTrack() {
     curr_track.play();
     isPlaying = true;
     track_art.classList.add('rotate');
     wave.classList.add('loader');
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-function pauseTrack(){
+
+function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
     track_art.classList.remove('rotate');
     wave.classList.remove('loader');
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
-function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
+
+function nextTrack() {
+    if (track_index < music_list.length - 1 && isRandom === false) {
         track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
+    } else if (track_index < music_list.length - 1 && isRandom === true) {
         let random_index = Number.parseInt(Math.random() * music_list.length);
         track_index = random_index;
-    }else{
+    } else {
         track_index = 0;
     }
     loadTrack(track_index);
     playTrack();
 }
-function prevTrack(){
-    if(track_index > 0){
+
+function prevTrack() {
+    if (track_index > 0) {
         track_index -= 1;
-    }else{
-        track_index = music_list.length -1;
+    } else {
+        track_index = music_list.length - 1;
     }
     loadTrack(track_index);
     playTrack();
 }
-function seekTo(){
+
+function seekTo() {
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
-function setVolume(){
+
+function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
-function setUpdate(){
+
+btnVol.forEach(el => {
+    el.addEventListener('click', evt => {
+        curr_track.volume = evt.target.value / 100
+        volume_slider.value = evt.target.value
+    })
+})
+
+btnVol.forEach(btn => {
+    btn.addEventListener('click', function () {
+        btnVol.forEach(btnn => btnn.classList.remove('btn-vol-active'));
+        this.classList.add('btn-vol-active')
+    })
+})
+
+function setUpdate() {
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
+    if (!isNaN(curr_track.duration)) {
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
 
@@ -161,10 +238,18 @@ function setUpdate(){
         let durationMinutes = Math.floor(curr_track.duration / 60);
         let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+        if (currentSeconds < 10) {
+            currentSeconds = "0" + currentSeconds;
+        }
+        if (durationSeconds < 10) {
+            durationSeconds = "0" + durationSeconds;
+        }
+        if (currentMinutes < 10) {
+            currentMinutes = "0" + currentMinutes;
+        }
+        if (durationMinutes < 10) {
+            durationMinutes = "0" + durationMinutes;
+        }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationSeconds;
